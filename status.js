f7dc8eeee3ad8897e32a4602016d55ed1a073348
Frank Watson <the.frank.watson@gmail.com>
2016-09-05 20:26:51 -0700

    Added 3rd server

--- status.js
+++ status.js
@@ -1,17 +1,18 @@
 var requesturl1 = "https://mcapi.us/server/status?ip=98.26.9.103";
 var requesturl2 = "https://mcapi.us/server/status?ip=98.26.9.103&port=25580";
+var requesturl3 = "https://mcapi.us/server/status?ip=98.26.9.103&port=25570";
 
 $.getJSON(requesturl1, function(json) {
     var html = '';
 
-    var status = json.status;
+    var online = json.online;
     var players_max = json.players.max
     var players_now = json.players.now
 
-    if(status == 'success'){
-        html += "Status: Online" + "<br /> Players Online: " + players_now
+    if(online == true){
+        html += "Players Online: " + players_now
     } else {
-        html += "Status: Offline"
+        html += "Offline"
     }
 
     htmlOutput1(html);
@@ -19,17 +20,32 @@ $.getJSON(requesturl1, function(json) {
 $.getJSON(requesturl2, function(json) {
     var html = '';
 
-    var status = json.status;
+    var online = json.online;
     var players_max = json.players.max
     var players_now = json.players.now
 
-    if(status == 'success'){
-        html += "Status: Online" + "<br /> Players Online: " + players_now
+    if(online == true){
+        html += "Players Online: " + players_now
     } else {
-        html += "Status: Offline"
+        html += "Offline"
     }
 
     htmlOutput2(html);
+});
+$.getJSON(requesturl3, function(json) {
+    var html = '';
+
+    var online = json.online;
+    var players_max = json.players.max
+    var players_now = json.players.now
+
+    if(online == true){
+        html += "Players Online: " + players_now
+    } else {
+        html += "Offline"
+    }
+
+    htmlOutput3(html);
 });
 
 function htmlOutput1(html) {
@@ -38,4 +54,6 @@ function htmlOutput1(html) {
 function htmlOutput2(html) {
     $('#modded').html(html);
 }
-
+function htmlOutput3(html) {
+    $('#crackpack').html(html);
+}
